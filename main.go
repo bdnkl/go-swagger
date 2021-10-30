8dbb9455dcd338aae640a51f1b7f656fe1bc5ecc
bdnkl <boas_d@web.de>
2021-10-30 19:31:07 +0200

    added api module

--- main.go
+++ main.go
@@ -1,8 +1,7 @@
 package main
 
 import (
-	"net/http"
-
+	"github.com/bdnkl/go-swagger/api"
 	_ "github.com/bdnkl/go-swagger/docs"
 
 	"github.com/gin-gonic/gin"
@@ -10,86 +9,13 @@ import (
 	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
-// album represents data about a record album.
-type album struct {
-	ID     string  `json:"id"`
-	Title  string  `json:"title"`
-	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
-}
-
-// albums slice to seed record album data.
-var albums = []album{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
-}
-
 func main() {
 	router := gin.Default()
-	router.GET("/albums", getAlbums)
-	router.GET("/albums/:id", getAlbumByID)
-	router.POST("/albums", postAlbums)
+	router.GET("/albums", api.GetAlbums)
+	router.GET("/albums/:id", api.GetAlbumByID)
+	router.POST("/albums", api.PostAlbums)
 
 	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 
 	router.Run("localhost:8080")
-}
-
-// @Summary Get albums
-// @Schemes
-// @Description
-// @Tags album
-// @Accept json
-// @Produce json
-// @Success 200 {object} []album
-// @Router /albums [get]
-func getAlbums(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, albums)
-}
-
-// @Summary Post album
-// @Schemes
-// @Description
-// @Tags album
-// @Param album body album true "Album info"
-// @Accept json
-// @Produce json
-// @Success 200 {object} album
-// @Router /albums [post]
-func postAlbums(c *gin.Context) {
-	var newAlbum album
-
-	// Call BindJSON to bind the received JSON to
-	// newAlbum.
-	if err := c.BindJSON(&newAlbum); err != nil {
-		return
-	}
-
-	// Add the new album to the slice.
-	albums = append(albums, newAlbum)
-	c.IndentedJSON(http.StatusCreated, newAlbum)
-}
-
-// @Summary Get album with id
-// @Schemes
-// @Description
-// @Tags album
-// @Accept json
-// @Produce json
-// @Param id path int true "id"
-// @Success 200 {object} album
-// @Router /albums/{id} [get]
-func getAlbumByID(c *gin.Context) {
-	id := c.Param("id")
-
-	// Loop through the list of albums, looking for
-	// an album whose ID value matches the parameter.
-	for _, a := range albums {
-		if a.ID == id {
-			c.IndentedJSON(http.StatusOK, a)
-			return
-		}
-	}
-	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
 }
